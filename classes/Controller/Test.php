3563e44c1d0751d964508eca0ed66fb3172161d4
Shtirlic <trafik8787@gmail.com>
2015-01-27 10:07:48 +0200

    fix bag save and apply test file

--- classes/Controller/Test.php
+++ classes/Controller/Test.php
@@ -22,35 +22,11 @@ class Controller_Test extends Controller
     public static function adminpanel()
     {
         $crud = new Cruds;
-        $crud->load_table('city');
-
-        $crud->disable_editor('name');
-        $crud->disable_editor('name_en');
-        $crud->disable_editor('region_en');
-        $crud->disable_editor('region');
-
-
-        $crud->show_name_column(array('id' => 'ID',
-            'name' => 'Название',
-            'name_en' => 'Название (анг.)',
-            'region' => 'Регион',
-            'region_en' => 'Регион (анг.)'));
-
-        $crud->set_lang('ru');
-        //$crud->icon_edit('glyphicon-pencil');
-        //$crud->icon_delete('glyphicon-remove-circle');
-        $crud->set_field_type('name', 'text');
-        $crud->set_field_type('name_en', 'text');
-        $crud->set_field_type('region', 'text');
-        $crud->set_field_type('region_en', 'text');
-
-
+        $crud->load_table('example');
 
         return $crud;
 
     }
-
-
 
 
 
